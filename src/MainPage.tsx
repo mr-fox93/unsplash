19cabf777b0b1196f9ae2bab7170b9b7ed366f9e
The Fox <lisieckikamil93@gmail.com>
2023-08-31 19:42:58 +0200

    IN-BUILD add for test response data on screen

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -2,25 +2,52 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const fetchUnsplash = async (searchTerm: string) => {
+interface UnsplashImage {
+  id: string;
+  error: string;
+
+  urls: {
+    small: string;
+  };
+  description: string;
+}
+
+interface UnsplashResponse {
+  results: UnsplashImage[];
+}
+
+const fetchUnsplash = async (searchTerm: string): Promise<UnsplashResponse> => {
   const apiKey = process.env.REACT_APP_MY_API_KEY;
-  const res = await axios.get(
+  const res = await axios.get<UnsplashResponse>(
     `https://api.unsplash.com/search/photos?query=${searchTerm}&client_id=${apiKey}`
   );
   console.log(res.data.results);
+  return res.data;
 };
 
 const MainPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const { data, isLoading, isError, error } = useQuery(
-    ["photos", searchTerm],
-    () => fetchUnsplash(searchTerm)
-  );
+  const { data, isLoading, isError, error, isSuccess } =
+    useQuery<UnsplashResponse>(
+      ["photos", searchTerm],
+      () => fetchUnsplash(searchTerm),
+      { enabled: !!searchTerm }
+    );
 
   return (
     <div>
-      <input type="text" onChange={(e) => setSearchTerm(e.target.value)} />
+      <input
+        value={searchTerm}
+        type="text"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {isLoading && <p>Ładowanie...</p>}
+      {isError && <p>Wystąpił błąd: {(error as Error).message}</p>}
+
+      {data?.results.map((photo) => (
+        <img key={photo.id} src={photo.urls.small} alt={photo.description} />
+      ))}
     </div>
   );
 };
