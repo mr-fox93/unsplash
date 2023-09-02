22e7070f539419a29d61078ae083d21fcb334bce
The Fox <lisieckikamil93@gmail.com>
2023-09-02 16:47:15 +0200

    IN-PROGRESS: add swich from mui to set up light/dark mode

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 import QuiltedPhotoList from "./QuiltedPhotoList";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import InputAdornment from "@mui/material/InputAdornment";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Stack from "@mui/material/Stack";
+import CircularProgress from "@mui/material/CircularProgress";
+import Switch from "@mui/material/Switch";
+import NightsStayIcon from "@mui/icons-material/NightsStay";
+import LightModeIcon from "@mui/icons-material/LightMode";
 
 export interface UnsplashImage {
   id: string;
@@ -52,6 +60,10 @@ const fetchUnsplash = async (searchTerm: string): Promise<UnsplashResponse> => {
 
 const MainPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [lightDarkMode, setLightDarkMode] = useState(true);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const { data, isLoading, isError, error } = useQuery<UnsplashResponse>(
     ["photos", searchTerm],
@@ -60,33 +72,108 @@ const MainPage = () => {
       cacheTime: 1000 * 60 * 60 * 24,
       staleTime: 1000 * 60 * 60 * 24,
     }
-    //{ enabled: !!searchTerm }
   );
 
-  return (
-    <div>
-      <TextField
-        margin="normal"
-        id="outlined-basic"
-        label="Find what you like..."
-        variant="outlined"
-        value={searchTerm}
-        type="text"
-        onChange={(e) => setSearchTerm(e.target.value)}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          ),
-        }}
-      />
+  //
+  useEffect(() => {
+    console.log("darkmode:", lightDarkMode);
+  }, [lightDarkMode]);
+  //
 
-      {isLoading && <p>Ładowanie...</p>}
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: isMobile ? "column" : "row",
+          justifyContent: isMobile ? "center" : "space-between",
+          alignItems: "center",
+          gap: isMobile ? "10px" : "0",
+          padding: ".5rem",
+        }}
+      >
+        <div
+          style={{
+            left: isMobile ? "0" : "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <GitHubIcon
+            fontSize="large"
+            style={{ color: "rgba(0, 0, 0, 0.6)" }}
+          />
+          <p style={{ marginLeft: "10px", color: "rgba(0, 0, 0, 0.6)" }}>
+            <a
+              style={{
+                textDecoration: "none",
+                color: "rgba(0, 0, 0, 0.6)",
+              }}
+              href="https://github.com/mr-fox93"
+              target="_blank"
+              rel="noreferrer"
+            >
+              mr-fox93
+            </a>
+          </p>
+        </div>
+        <TextField
+          margin="normal"
+          id="outlined-basic"
+          label="Find what you like..."
+          variant="outlined"
+          value={searchTerm}
+          type="text"
+          onChange={(e) => setSearchTerm(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            {lightDarkMode ? (
+              <LightModeIcon style={{ color: "rgba(0, 0, 0, 0.6)" }} />
+            ) : (
+              <NightsStayIcon style={{ color: "rgba(0, 0, 0, 0.6)" }} />
+            )}
+          </div>
+          <Switch
+            onChange={() => setLightDarkMode(!lightDarkMode)}
+            defaultChecked={false}
+            color="default"
+          />
+        </div>
+      </div>
+
+      {isLoading && (
+        <Stack
+          sx={{
+            color: "rgba(0, 0, 0, 0.6)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "50px",
+          }}
+          spacing={6}
+          direction="row"
+        >
+          <CircularProgress color="inherit" />
+        </Stack>
+      )}
       {isError && <p>Wystąpił błąd: {(error as Error).message}</p>}
 
       {data && <QuiltedPhotoList photos={data.results} />}
-    </div>
+    </>
   );
 };
 
